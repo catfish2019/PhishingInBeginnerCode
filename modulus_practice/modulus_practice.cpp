9c8a2fbf7356b09d88e9ed126b7fb085dd972283
Catherine Psarakis <psarakis.c@husky.neu.edu>
2019-07-20 14:20:44 -0400

    my message

--- modulus_practice/modulus_practice.cpp
+++ modulus_practice/modulus_practice.cpp
@@ -7,7 +7,8 @@ int main () {
 	int var = 0;
 	bool terminate = false; 
 	int modulus = 0; 	
-
+	string notes[12] = {"c", "c#", "d", "d#", "e", "f","f#" "g", "g#", "a", "a#","b"};
+	
 	do {
 
 	cout << "input a semitone. Press 0 to escape" << endl; 	
@@ -18,8 +19,15 @@ int main () {
 	if (var == 0)
 		terminate = true; 
 	
-	else if string notes[12] = {"c", "c#", "d", "d#", "e", "f","f#" "g", "g#", "a", "a#","b"};	
+	else if (var > 88)
+		terminate = true; 
+
+	else if (var < 1)
+		terminate = true;
+	
+	else {	
 		cout << notes [modulus] << endl;	
+		}
 
 	} while (!terminate ); 
 	cout << "you have now quit" << endl;
